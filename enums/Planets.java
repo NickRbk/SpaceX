57c2e899080dbc2f8ebfc98a5c34935ee226e1a8
Nick Rybak <nrybak94@gmail.com>
2018-03-21 20:00:01 +0200

    dev: add enums folder and create Planets enum with distance and name props

---
+++ enums/Planets.java
@@ -0,0 +1,22 @@
+package enums;
+
+public enum Planets {
+    MARS(100, "Mars"), MOON(4, "Moon");
+
+    private int destination;
+    private String name;
+
+    Planets(int destination, String name) {
+        this.destination = destination;
+        this.name = name;
+    }
+
+    // set getters for destination and name
+    public int getDestination() {
+        return destination;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
