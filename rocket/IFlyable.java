13e5dd893f93484e14573f428687f04b431714b2
Nick Rybak <nrybak94@gmail.com>
2018-03-23 22:09:19 +0200

    dev: simplify IFlyable interface

--- rocket/IFlyable.java
+++ rocket/IFlyable.java
@@ -8,9 +8,7 @@ public interface IFlyable {
     // define method same to all space rocket
     // calc is you can access your destination
     default void isSuccessFly(Planets planet, int tankVolume, int fuelConsumption) {
-        int fuelNeeds = Engine.getFuelNeeds(fuelConsumption, planet.getDestination());
-
-        if(fuelNeeds > tankVolume) {
+        if(Engine.getFuelNeeds(fuelConsumption, planet.getDestination()) > tankVolume) {
             System.out.println("\nOooops, your rocket crashed... You needed additionally "
                     + Engine.getAdditionalFuelNeeds(fuelConsumption, planet.getDestination(), tankVolume)
                     + " fuel units");
