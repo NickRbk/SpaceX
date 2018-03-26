32e39c643f099487bdc474f0862a56e86e24dd2d
Nick Rybak <nrybak94@gmail.com>
2018-03-27 02:56:25 +0300

    dev: minor changes in properties to simulates different situations

--- rocket/IPotentiallyFlyable.java
+++ rocket/IPotentiallyFlyable.java
@@ -11,10 +11,10 @@ public interface IPotentiallyFlyable {
         BigDecimal fuelNeeds = SpaceCalculations.fuelNeeds(distanceMeters, acceleration, totalWeight, efficiency);
 
         if(BigDecimal.valueOf(totalFuel).compareTo(fuelNeeds) > 0) {
-            System.out.println("You have enough fuel for travel, good luck!");
+            System.out.println("\n\tYou have enough fuel for travel, good luck!");
             return true;
         } else {
-            System.out.println("You haven`t enough fuel for travel, you need more "
+            System.out.println("\nYou haven`t enough fuel for travel, you need more "
                     + fuelNeeds.subtract(BigDecimal.valueOf(totalFuel)).toBigInteger());
             return false;
         }
